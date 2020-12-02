0dfdab9cf806f61e40723a179633aae51ed65fc1
lmisselyn <lambert.misselyn@student.uclouvain.be>
2020-12-02 12:54:13 +0100

    sem

--- srcs/synchro.c
+++ srcs/synchro.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+
 
 int test_set(volatile int *lock, int lock_val)
 {
@@ -35,114 +37,31 @@ void lock_tts(volatile int *lock)
   }
 }
 
-/*
+typedef struct sem {
+  volatile int nb;
+  volatile int mutex;
+} semaphore_t;
 
-struct sem
-{
-  volatile int count;
-  volatile int lock;
-};
-
-//init sem at initial value
-struct sem *seminit(int initial_value)
-{
-  struct sem *ret = (struct sem *)malloc(sizeof(struct sem));
-  ret->count = initial_value;
-  ret->lock = 0;
-
-  return ret;
-}
-
-//wait until running state available
-void semwait(struct sem *sem)
-{
-  if (sem->count > 0)
-  {
-    lock_ts(&sem->lock);
-    sem->count = (sem->count) - 1;
-    unlock_ts(&sem->lock);
+void seminit(semaphore_t* new_sem, volatile int nb) {
+  if (nb < 0) {
+    perror("arg cannot be negative");
   }
-  else
-  {
-    semwait(sem);
+  new_sem->nb = nb;
+  new_sem->mutex = 0;
+}
+void semwait(semaphore_t* sem) {
+  if (sem->nb > 0) {
+    sem->nb = sem->nb-1;
+  }
+  else {
+    lock_ts(&(sem->mutex));
   }
 }
 
-//increment available spot
-void sempost(struct sem *sem)
-{
-  lock_ts(&sem->lock);
-  sem->count = (sem->count) + 1;
-  unlock_ts(&sem->lock);
-}
-
-//destroy sem
-void semdestroy(struct sem *sem)
-{
-  free(sem);
-}
-
-*/
-
-typedef struct primitive_sem
-{
-  int val;
-  volatile int* lock;
-}prim_sem;
-
-//Initialize the sem structure and his lock
-int prim_sem_init(prim_sem **s, int start_val)
-{
-  *s = (struct primitive_sem *)malloc(sizeof(struct primitive_sem));
-  if (*s == NULL) return -1;
-  (**s).lock = (int *)malloc(sizeof(int));
-  *((**s).lock) = 0;
-  (*s)->val = start_val;
-  return 0;
-}
-
-//free the sem struct
-int prim_sem_destroy(prim_sem *sem)
-{
-  free((int *)(sem->lock));
-  free(sem);
-  return 0;
-}
-
-//takes sem structure as an argument
-//puts the threads in spinlock if the val is not >0 and decrements the val of sem once out of the spinlock
-int prim_sem_wait(prim_sem *sem)
-{
-  bool cond = false;
-
-  while (!cond)
-  {
-    lock_tts(sem->lock);
-
-    if (sem->val > 0)
-    {
-      //int val = *(sem->lock);
-      //printf("before name: %s val: %d and lock: %d\n", name, sem->val, *(sem->lock));
-      //printf("%d\n", val);
-      sem->val = sem->val - 1;
-      //printf("after name: %s val: %d and lock: %d\n", name, sem->val, *(sem->lock));
-      cond = true;
-      unlock_ts(sem->lock);
-    }
-    else
-    {
-      unlock_ts(sem->lock);
-    }
+void sempost(semaphore_t* sem) {
+  if(sem->nb == 0) {
+    unlock_ts(&(sem->mutex));
   }
-  return 0;
+  sem->nb = sem->nb+1;
 }
 
-//takes sem structure as an argument
-//increments the val of sem
-int prim_sem_post(prim_sem *sem)
-{
-  lock_tts(sem->lock);
-  sem->val = sem->val + 1;
-  unlock_ts(sem->lock);
-  return 0;
-}
